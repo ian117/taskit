19756c70902a685ff95279ff58ae82fd17031596
Moises Fuentes <moyxess@gmail.com>
2021-07-10 14:00:56 -0500

    pull conflict resolved

--- server.js
+++ server.js
@@ -5,25 +5,18 @@ const path = require(`path`);
 const PORT = process.env.PORT;
 const app = express();
 
-<<<<<<< HEAD
 //Middleware incorporado para compartir de manera pública
 app.use(express.static(path.join(__dirname, `public`)));
-=======
->>>>>>> 3bb8c9890afa893be4b859f6b08b40989422bfb2
 
 app.set(`views`, path.join(__dirname, `views`));
 app.set(`view engine`, `ejs`);
 
 
-<<<<<<< HEAD
 //Página principal: home
-=======
->>>>>>> 3bb8c9890afa893be4b859f6b08b40989422bfb2
 app.get(`/`, (request, response, next) => {
     response.render(`pages/home`, {title: `Inicio`});
 });
 
-<<<<<<< HEAD
 //Página para logearse: login
 app.get(`/login`, (request, response, next) => {
     response.render(`pages/login`, {title: `Iniciar Sesión`});
@@ -36,8 +29,6 @@ app.get(`/register`, (request, response, next) => {
 
 
 
-=======
->>>>>>> 3bb8c9890afa893be4b859f6b08b40989422bfb2
 
 app.listen(PORT, () => {
     console.log(`El servidor está escuchando sobre el puerto ${PORT}`);
