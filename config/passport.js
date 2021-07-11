89f655fdead5f7dd570e8a4c55aebcbb13644d94
ian117 <spideramerica71@gmail.com>
2021-07-11 01:59:38 -0500

    table sessions, and passport.js strategies

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@
 const bcrypt = require(`bcrypt`);
 const passport = require(`passport`);
 const LocalStrategy = require(`passport-local`).Strategy;
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const { Users } = require(`../models`);
 
 passport.use(new LocalStrategy({
@@ -23,22 +25,64 @@ passport.use(new LocalStrategy({
     }
 }));
 
+
+//Estrategia Google OAuth 2.0
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_REDIRECT_URI
+}, (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+}));
+
+
+//Estrategia Facebook
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.FB_CLIENTID,
+//     clientSecret: process.env.FB_SECRET,
+//     callbackURL: process.env.FB_REDIRECT_URI
+//   },
+//   (accessToken, refreshToken, profile, done) => {
+//     return done(null, profile);
+//   }
+// ));
+
+
+
 //Serialización
 //firmar los datos del usuario
-passport.serializeUser((user, done) => {
-    return done(null, user.id);
+passport.serializeUser((profile, done) => {
+    //Firmar los datos del usuario
+    return done(null, profile);
 });
+
 
 //Desialización
 //Obtener los datos del usuario a partir del id
-passport.deserializeUser(async (id, done) => {
-    try {
-        let user = await Users.findByPk(id, { plain: true });
-        //request -> request.user
-        done(null, user);
-    } catch (error) {
-        return done(error);
+passport.deserializeUser(async (profile, done) => {
+  //Vamos a obtener los datos del usuario a partir del ID
+  try{
+    switch(profile.provider){
+        case 'google': 
+            //Generado por google
+            profile.firstname = profile.name.givenName;
+            profile.lastname = profile.name.familyName;
+            done(null, profile);
+            break;
+        case 'facebook':
+            profile.firstname = profile.displayName;
+            profile.lastname = "";
+            done(null, profile);
+            break;
+        default:
+            let user = await Users.findByPk(profile.id, {plain: true});
+            done(null, user); //request -> request.user
+            break;
     }
+}catch(error){
+    done(error);
+}
+
 });
 
 
