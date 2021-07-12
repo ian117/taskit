7b249110ec023f17049a52dd3436d5b7e1cbc087
ian117 <spideramerica71@gmail.com>
2021-07-12 13:09:00 -0500

    Deploy changes

--- config/config.js
+++ config/config.js
@@ -14,17 +14,29 @@ module.exports = {
     loggin: true
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABSE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    define: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    loggin: true
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+   use_env_variable:process.env.DATABASE_URL,
+    define: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    loggin: false,
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  },
   }
 }
