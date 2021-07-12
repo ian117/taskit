5df9c798443af9ec6fae7324c1a335b6fe838c07
ian117 <spideramerica71@gmail.com>
2021-07-12 13:10:37 -0500

    use env var

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ module.exports = {
     loggin: true
   },
   production: {
-   use_env_variable:process.env.DATABASE_URL,
+   use_env_variable:'DATABASE_URL',
     define: {
       createdAt: 'created_at',
       updatedAt: 'updated_at'
